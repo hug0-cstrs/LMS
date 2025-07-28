3d2b1ede12988e62201963bf5ce891bfaedbdccb
hug0-cstrs <hugo.casteras@outlook.fr>
2025-07-29 01:45:39 +0200

    ✨ Ajout de l'authentification avec GitHub, début de l'intégration de Better Auth

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -8,9 +10,32 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { GithubIcon } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
+import { GithubIcon, Loader2 } from "lucide-react";
+import { useTransition } from "react";
+import { toast } from "sonner";
 
 export default function LoginPage() {
+  const [githubPending, startGithubTransition] = useTransition();
+
+  async function signInWithGithub() {
+    startGithubTransition(async () => {
+    await authClient.signIn.social({
+      provider: "github",
+      callbackURL: "/",
+      fetchOptions: {
+        onSuccess: () => {
+          toast.success(
+            "Signed in with Github, you will be redirected to the home page"
+          );
+        },
+        onError: (error) => {
+          toast.error("Internal server error");
+          },
+        },
+      });
+    });
+  }
   return (
     <Card>
       <CardHeader>
@@ -21,9 +46,23 @@ export default function LoginPage() {
       </CardHeader>
 
       <CardContent className="flex flex-col gap-4">
-        <Button variant="outline" className="w-full">
-          <GithubIcon className="size-4" />
-          Sign in with Github
+        <Button
+          onClick={signInWithGithub}
+          variant="outline"
+          className="w-full"
+          disabled={githubPending}
+        >
+          {githubPending ? (
+            <>
+              <Loader2 className="size-4 animate-spin" />
+              <span>Loading...</span>
+            </>
+          ) : (
+            <>
+              <GithubIcon className="size-4" />
+              Sign in with Github
+            </>
+          )}
         </Button>
 
         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
@@ -34,14 +73,8 @@ export default function LoginPage() {
 
         <div className="grid gap-3">
           <div className="grid gap-2">
-            <Label htmlFor="email">
-              Email
-            </Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@exemple.com"
-            />
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="email" placeholder="m@exemple.com" />
           </div>
 
           <Button className="w-full">Continue with Email</Button>

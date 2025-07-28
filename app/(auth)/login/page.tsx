2ed7580ed57d69836fb25248310c9cb233e5b85a
hug0-cstrs <hugo.casteras@outlook.fr>
2025-07-28 23:40:10 +0200

    Initial commit

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function LoginPage() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Welcome back</CardTitle>
+      </CardHeader>
+    </Card>
+  );
+}

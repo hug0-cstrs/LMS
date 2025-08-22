97d8d085c438be91b378b4acb340205d79cfa0a0
hugo.casteras <hugo.casteras@groupeactual.eu>
2025-08-22 14:27:14 +0200

    ✨ Ajout des composants PublicCourseCard et PublicCourseCardSkeleton pour l'affichage des cours, intégration de la logique de récupération des cours et amélioration de la page des cours publics avec un chargement dynamique.

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -3,9 +3,11 @@ import { Navbar } from "./_components/Navbar";
 
 export default function LayoutPublic({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="">
       <Navbar />
-      <main className="container mx-auto px-4 md:px-6 lg:px-8">{children}</main>
+      <main className="container mx-auto px-4 md:px-6 lg:px-8 mb-32">
+        {children}
+      </main>
     </div>
   );
 }
---
+++ app/data/course/get-course.ts
@@ -0,0 +1,46 @@
+import { prisma } from "@/lib/db";
+import { notFound } from "next/navigation";
+
+export async function getIndividualCourse(slug: string) {
+  const course = await prisma.course.findUnique({
+    where: {
+      slug: slug,
+    },
+    select: {
+      title: true,
+      price: true,
+      smallDescription: true,
+      description: true,
+      slug: true,
+      fileKey: true,
+      id: true,
+      level: true,
+      duration: true,
+      category: true,
+      chapter: {
+        select: {
+          id: true,
+          title: true,
+          lessons: {
+            select: {
+              id: true,
+              title: true,
+            },
+            orderBy: {
+              position: "asc",
+            },
+          },
+        },
+        orderBy: {
+          position: "asc",
+        },
+      },
+    },
+  });
+
+  if (!course) {
+    return notFound();
+  }
+
+  return course;
+}
